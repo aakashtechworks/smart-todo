8ef77bf40cccd327ac4a159504f8b7230bb622de
Aakash Gupta <aakashtechworks@gmail.com>
2026-07-31 11:14:49 +0530

    create folder stractures

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-red-800 text-white text-5xl font-bold text-center'>Hello React</div>
+  )
+}
+
+export default App
