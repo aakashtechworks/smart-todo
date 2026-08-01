ed5f03ecf5b5def35f7bba62b0ed965d6b715dec
Aakash Gupta <aakashtechworks@gmail.com>
2026-08-01 10:04:52 +0530

    set up react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import MainLayout from './layout/MainLayout'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
 
 const App = () => {
   return (
-    <div className='bg-red-800 text-white text-5xl font-bold text-center'>Hello React</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+const MainLayout = () => {
+  return (
+    <div>
+        <h2>Header</h2>
+        <Outlet />
+        <h2>Footer</h2>
+    </div>
+  )
+}
+
+export default MainLayout
