787cf93625a467c04abbe81dd87a69e030a1cd5a
Aakash Gupta <aakashtechworks@gmail.com>
2026-08-03 09:37:44 +0530

    build responsive nabvar with react router and use state

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -13,10 +13,25 @@ const Header = () => {
         <nav className='max-w-6xl mx-auto flex justify-between items-center px-6 py-4'>
             <h1 className='text-2xl font-bold'>Smart Todo</h1>
 
-            <button onClick={toggleMenu}>{isMenuOpen ? "x" : "="}</button>
+            <ul className='hidden lg:flex gap-6 '>
+                <li ><NavLink to="/"  className={({isActive})=> isActive? "text-sky-400 font-semibold" : "text-white hover:text-sky-300 transition-colors"}>
+                    Home
+                    </NavLink>
+                </li>
+                <li ><NavLink to="/about" className={({isActive})=> isActive? "text-sky-400 font-semibold" : "text-white hover:text-sky-300 transition-colors"}>
+                    About
+                    </NavLink>
+                </li>
+                <li ><NavLink to="/contact"  className={({isActive})=> isActive? "text-sky-400 font-semibold" : "text-white hover:text-sky-300 transition-colors"}>
+                    Contact
+                    </NavLink>
+                </li>
+                </ul>
+
+            <button className='lg:hidden' onClick={toggleMenu}>{isMenuOpen ? "x" : "="}</button>
         </nav>
             {isMenuOpen && (
-                <ul className='flex flex-col gap-6 '>
+                <ul className='flex flex-col gap-6 lg:hidden '>
                 <li ><NavLink to="/" onClick={()=>setIsMenuOpen(false)} className={({isActive})=> isActive? "text-sky-400 font-semibold" : "text-white hover:text-sky-300 transition-colors"}>
                     Home
                     </NavLink>
